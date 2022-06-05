0ac03b020ac0ca51616a213100fc27d14d912eed
FATIH <FATIH@ASUS>
2022-06-05 14:19:04 +0300

    Add project files.

---
+++ WFA040622-G3/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace WFA040622_G3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.btnKur = new System.Windows.Forms.Button();
+            this.btnDurdur = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblMesaj = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker1.Location = new System.Drawing.Point(122, 34);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(102, 30);
+            this.dateTimePicker1.TabIndex = 0;
+            
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // btnKur
+            // 
+            this.btnKur.Location = new System.Drawing.Point(254, 39);
+            this.btnKur.Name = "btnKur";
+            this.btnKur.Size = new System.Drawing.Size(75, 23);
+            this.btnKur.TabIndex = 1;
+            this.btnKur.Text = "Alarm Kur";
+            this.btnKur.UseVisualStyleBackColor = true;
+            this.btnKur.Click += new System.EventHandler(this.btnKur_Click);
+            // 
+            // btnDurdur
+            // 
+            this.btnDurdur.Location = new System.Drawing.Point(355, 39);
+            this.btnDurdur.Name = "btnDurdur";
+            this.btnDurdur.Size = new System.Drawing.Size(92, 23);
+            this.btnDurdur.TabIndex = 2;
+            this.btnDurdur.Text = "Alarm Durdur";
+            this.btnDurdur.UseVisualStyleBackColor = true;
+            this.btnDurdur.Click += new System.EventHandler(this.btnDurdur_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Alarm Zamanı:";
+            // 
+            // lblMesaj
+            // 
+            this.lblMesaj.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblMesaj.Location = new System.Drawing.Point(165, 87);
+            this.lblMesaj.Name = "lblMesaj";
+            this.lblMesaj.Size = new System.Drawing.Size(191, 31);
+            this.lblMesaj.TabIndex = 4;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Coral;
+            this.ClientSize = new System.Drawing.Size(495, 142);
+            this.Controls.Add(this.lblMesaj);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDurdur);
+            this.Controls.Add(this.btnKur);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button btnKur;
+        private System.Windows.Forms.Button btnDurdur;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblMesaj;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WFA040622-G3/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WFA040622_G3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        SoundPlayer play;
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnKur_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            btnKur.Enabled = false;
+            lblMesaj.Text = "Alarm Kuruldu!";
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DateTime simdikiZaman = DateTime.Now;
+            DateTime alarmSaati = dateTimePicker1.Value;
+
+            if (simdikiZaman.Hour == alarmSaati.Hour && simdikiZaman.Minute == alarmSaati.Minute && simdikiZaman.Second == alarmSaati.Second)
+            {
+                timer1.Stop();
+                lblMesaj.Text = "Alarm Çalıyor...";
+                Stream alarm = Properties.Resources.alarm;
+                play = new SoundPlayer(alarm);
+                play.PlayLooping();
+
+            }
+
+        }
+
+        private void btnDurdur_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            btnDurdur.Enabled = false;
+            lblMesaj.Text = "Alarm Durduruldu!";
+            btnKur.Enabled = true;
+            play.Stop();
+
+        }
+    }
+}
